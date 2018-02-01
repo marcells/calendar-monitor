2246562bde12a91c9119b5de9d02d2cf1ec2c980
Marcell Spies <marcells@gmx.de>
2018-02-01 01:57:55 +0100

    Use expression syntax.

--- app/routes/api.js
+++ app/routes/api.js
@@ -2,14 +2,14 @@ var express = require('express');
 var router = express.Router();
 var data = require('../services/data');
 
-router.get('/calendar/:year/:month', function(req, res, next) {
+router.get('/calendar/:year/:month', (req, res, next) => {
   const year = parseInt(req.params.year);
   const month = parseInt(req.params.month);
 
   res.send({ events : data });
 });
 
-router.get('/upcoming', function(req, res, next) {
+router.get('/upcoming', (req, res, next) => {
   res.send({ events : data });
 });
 
