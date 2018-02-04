8d4ff3851fb6560e1a09e395758269d48c88f092
Marcell Spies <marcells@gmx.de>
2018-02-04 13:22:24 +0100

    Refactored file structure.

--- app/client/src/App.js
+++ app/client/src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Calendar from './components/Calendar/Calendar';
 import './App.css';
 
 class App extends Component {
@@ -21,54 +22,6 @@ class App extends Component {
       </div>
     );
   }
-}
-
-function Calendar(props) {
-  return (
-    <div className="Calendar">
-      <CalendarHeader date={props.date} />
-      <CalendarDays date={props.date} />
-    </div>
-  );
-}
-
-function CalendarDays(props) {
-  const lastDayOfMonth = new Date(props.date.year, props.date.month + 1, 0).getDate();
-  const days = Array(lastDayOfMonth).fill().map((e,i)=> i + 1);
-
-  return (
-      <div className="Calendar-days">
-        { days.map(x => <CalendarDay key={x} year={props.date.year} month={props.date.month} day={x} />) }
-      </div>
-    );
-}
-
-function CalendarHeader(props) {
-  const monthNames = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
-  const weekDayNames = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-  
-  return (
-    <div className="Calendar-header">
-      <div className="Calendar-header-month">
-        { monthNames[props.date.month] }
-      </div>
-      <div className="Calendar-header-weekdays">
-        { weekDayNames.map(x => <div>{ x }</div>)}
-      </div>
-    </div>
-  );
-}
-
-function CalendarDay(props) {
-  const gridColumnStyle = (props.day === 1) 
-    ? { gridColumn : new Date(props.year, props.month, 1).getDay() + 1 }
-    : { };
-
-  return (
-    <div className="Calendar-day" style={gridColumnStyle}>
-      { props.day }
-    </div>
-  );
 }
 
 export default App;
