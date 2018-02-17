d99e8bbbab862a4e5dc7bd361bc0f2cdfe99e6d9
Marcell Spies <marcells@gmx.de>
2018-02-17 18:31:54 +0100

    Create unique id for each event and map tags.

--- app/crawler/CalendarCrawler.js
+++ app/crawler/CalendarCrawler.js
@@ -2,35 +2,47 @@ class CalendarCrawler {
     constructor(calendar, tags) {
         this._calendar = calendar;
         this._tags = tags;
-        this._providerInstances = [];
-        this._timers = [];
-    }
-
-    async parseData(provider) {
-        const events = await provider.getEvents();
-
-        console.log(events);
-    }
-
-    getEvents() {
+        this._timer = null;
     }
 
     start() {
-        for (const provider of this._calendar.providers) {
-            const providerClass = require('../providers/' + provider.name);
-            const providerInstance = new providerClass(provider.configuration);
-            
-            this._providerInstances.push(providerInstance);
-
-            const timerForProvider = setInterval(
-                async () => await this.parseData(providerInstance),
-                this._calendar.intervalInSeconds * 1000);
-
-            this._timers.push(timerForProvider);
-        }
+        this._timer = setInterval(async () => this._getEvents(), this._calendar.intervalInSeconds * 1000);
     }
 
     stop() {
+        clearInterval(this._timer);
+        this._timer = null;
+    }
+
+    async _getEvents() {
+        for (const providerConfig of this._calendar.providers) {
+            const provider = this._createProvider(providerConfig);
+            
+            await this._parseData(providerConfig, provider);
+        }
+    }
+
+    _createProvider(providerConfig) {
+        const providerClass = require('../providers/' + providerConfig.provider);
+    
+        return new providerClass(providerConfig.configuration);
+    }
+
+    async _parseData(providerConfig, provider) {
+        const events = await provider.getEvents();
+
+        events.forEach(event => this._applyForEvent(providerConfig, event));
+
+        events.forEach(x => console.log(x.id, x.tags));
+    }
+
+    _applyForEvent(providerConfig, event) {
+        event.id = `${this._calendar.id}/${providerConfig.id}/${event.id}`;
+
+        event.tags = providerConfig.tags.map(tag => ({
+            name: tag,
+            properties: this._tags[tag]
+        }))
     }
 }
 
